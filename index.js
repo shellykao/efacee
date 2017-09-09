063a8501e6b070aad8ecf20ed148ddd21717e1b8
yifang <opopen2001@gmail.com>
2017-09-09 15:25:06 +0800

    3

--- index.js
+++ index.js
@@ -156,37 +156,7 @@ app.get('/verify',function(req,res){
 	}
 });
 
-app.post('/clothes/detail', function(request, response){
-	var id=mongodb.ObjectID(request.body.id);
-	var collection = myDB.collection('clothes');
-	collection.findOne({'_id': id},function(err, docs) {
-		if (err) {
-			response.status(406).end();
-		} else {
-			response.type('application/json');
-			response.status(200).send(docs);
-			response.end();
-		}
-	});
-});
 
-app.post('/clothes/simple', function(request, response){
-	var collection = myDB.collection('clothes');
-	cursor = collection.find({},{_id:true,user:true,price:true,image:true})
-							.sort({"rate_avg":-1})
-							.skip(parseInt(request.body.SkipCount)*10)
-							.limit(10);
-	cursor.toArray(function(err, docs) {
-		if (err) {			
-			console.log(err);
-			response.status(406).end();
-		} else {	
-			response.type('application/json');
-			response.status(200).send(docs);
-			response.end();
-		}
-	});	
-});
 
 
 mongodb.MongoClient.connect(mongodbURL, function(err, db){ //使用mongodb.MongoClient的方法connect()進行連線
