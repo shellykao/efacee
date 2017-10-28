732d18e00af2373e467012a3c4f07d2816e871c4
kaoyu <shelly011793@gmail.com>
2017-10-28 23:19:27 +0800

    2

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ var rand;
 var mailOptions;
 var mongodbURL =
 'mongodb://ABCDEF:a103221070@ds013232.mlab.com:13232/abcd'; //將MongoDB的位置在Server程式碼中以一個變數儲存
+var nfc_no,nfc_user,nfc_price,nfc_s,nfc_m,nfc_l;
 var myDB; //建立一個全域變數myDB
 app.set('port', (process.env.PORT || 5000));
 
@@ -74,6 +75,47 @@ app.post('/register', function(request, response){
 		}
 	});
 });
+
+app.post('/nfc',function(request, response){
+	
+	nfc_no = request.body.Number;
+	nfc_user=request.body.User;
+	nfc_price=request.body.Price;
+	nfc_s=request.body.Ss;
+	nfc_m=request.body.Ms;
+	nfc_l=request.body.Ls;
+
+    console.log(nfc_no);
+	console.log(nfc_user);
+	console.log(nfc_price);
+	console.log(nfc_s);
+	console.log(nfc_m);
+	console.log(nfc_l);
+
+
+	
+	var collection = myDB.collection('product');
+	
+	collection.find({"number":nfc_no}).toArray(function(err, docs) {
+		if (err) {
+			response.status(406).end();
+		} else {
+			
+			response.type('application/json');
+			response.status(200).send(docs);
+			response.end();
+			
+			nfc_no = null;
+			nfc_user = null;
+			nfc_price = null;
+			nfc_s = null;
+			nfc_m = null;
+			nfc_l = null;
+
+		}
+	});
+});
+
 
 
 //插入使用者資料
